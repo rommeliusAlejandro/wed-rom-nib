738d2d4518a8b243a856c7c7dd8df3d09eae8fe5
rloayza <rommel.loayza@dharbor.com>
2024-02-24 16:43:23 -0400

    confirmation form styling

--- src/app/invitation/services/whatsapp-sender.service.ts
+++ src/app/invitation/services/whatsapp-sender.service.ts
@@ -9,15 +9,12 @@ export class WhatsappSenderService {
   constructor() { }
 
   public sendMessage(type: ConfirmationFormType): void {
-    console.log(type);
-
     const whatsappUrl = `https://wa.me/75944185/?text=`;
     const messagePositive = `Hola! muchas gracias por la invitación. Mi nombre es *${type.fullName}* y mi número es *${type.phoneNumber}* y *Si* asistiré a la boda.`;
     const messageNegative = `Hola! muchas gracias por la invitación. Mi nombre es *${type.fullName}* y *No* asistiré a la boda.`;
 
     let text = 'failure';
 
-    console.log(typeof  type.confirm);
     if(type.confirm === 'Si') {
       text = messagePositive;
     }
