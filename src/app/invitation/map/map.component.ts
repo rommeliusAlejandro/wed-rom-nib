d4aeda1996770f1a417262f4792a4a6e4b2375ab
rloayza <rommel.loayza@dharbor.com>
2024-02-24 20:38:43 -0400

    fixing styling issues.

--- src/app/invitation/map/map.component.ts
+++ src/app/invitation/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -9,7 +9,8 @@ export class MapComponent implements OnInit {
 
   alertHidden: boolean = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
